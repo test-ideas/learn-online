4af4036b568d7661919aebe40a63399f356921ea
test-ideas <arrangeappointmets@gmail.com>
2024-05-12 11:53:56 +0000

    EleventyHtmlBasePlugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,9 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
